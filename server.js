e0694c078cbdaec6a5d390d6349f0cc822ccf638
Willie <83033083+Banehub@users.noreply.github.com>
2025-10-24 13:32:14 +0200

    removed junk

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const quitTrackingRoutes = require('./routes/quit-tracking');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors());
